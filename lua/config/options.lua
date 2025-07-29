c848554e0f0c5092a6041f9522ed174f653d5b02
Joe Mills <joe.mills@mheducation.com>
2025-07-29 10:14:17 -0500

    Fixing mason and LSP

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+require("noice").setup({
+  timeout = 4500,
+  statges = "static",
+})
+
+vim.g.root_spec = { "cwd" }
